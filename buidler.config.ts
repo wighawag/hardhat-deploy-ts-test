ed5dc455e7d2a565c2c0ea42ba43b98e85ceadbc
Wighawag <wighawag@gmail.com>
2020-04-07 10:23:13 +0100

    TODO maek it work with getContract

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,5 +1,6 @@
 import { task, usePlugin, BuidlerConfig } from "@nomiclabs/buidler/config";
 usePlugin("buidler-ethers-v5");
+usePlugin("buidler-deploy");
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
@@ -7,12 +8,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, bre) => {
   const accounts = await bre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account);
+    console.log(await account.getAddress());
   }
 });
 
 const config: BuidlerConfig = {
-  
+  namedAccounts: {
+    deployer: 0
+  }
 };
 
 export default config;
---
+++ deploy/000_deploy_greeter.ts
@@ -0,0 +1,11 @@
+import { BuidlerRuntimeEnvironment, DeployFunction } from "@nomiclabs/buidler/types";
+
+const func: DeployFunction = async function(bre : BuidlerRuntimeEnvironment) {
+    const {deployments, namedAccounts} = bre;
+    const {
+        deployer
+    } = namedAccounts;
+    const {deploy} = deployments;
+    await deploy("Greeter", {from: deployer}, "Greeter", "hi");
+}
+export default func;
