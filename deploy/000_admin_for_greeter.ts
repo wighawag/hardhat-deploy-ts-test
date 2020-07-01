6ce6b87adbaed6a5923e424806ad24565f96be48
Wighawag <wighawag@gmail.com>
2020-07-01 20:00:18 +0100

    tweaks

--- deploy/000_admin_for_greeter.ts
+++ deploy/000_admin_for_greeter.ts
@@ -1,14 +1,19 @@
+import {
+  BuidlerRuntimeEnvironment,
+  DeployFunction,
+} from "@nomiclabs/buidler/types";
 
-import { BuidlerRuntimeEnvironment, DeployFunction } from "@nomiclabs/buidler/types";
+const func: DeployFunction = async function (bre: BuidlerRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = bre;
+  const { execute, log, read } = deployments;
+  const { deployer, admin } = await getNamedAccounts();
 
-const func: DeployFunction = async function(bre : BuidlerRuntimeEnvironment) {
-    const {deployments, getNamedAccounts} = bre;
-    const {execute, log, read} = deployments;
-    const {deployer, admin} = await getNamedAccounts();
-
-    const currentAdmin = await read("Greeter", "getAdmin");
-    await execute("Greeter", {from: currentAdmin}, "setAdmin", admin);
+  const currentAdmin = await read("Greeter", "getAdmin");
+  if (currentAdmin !== admin) {
+    log(`setting admin from ${currentAdmin} to ${admin}...`);
+    await execute("Greeter", { from: currentAdmin }, "setAdmin", admin);
     log(`admin set to ${admin}`);
-}
+  }
+};
 export default func;
 func.runAtTheEnd = true;
