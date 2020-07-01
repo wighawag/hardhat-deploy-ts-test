ff2e3d37d2b6e94fe20d59c170868376763d1833
Wighawag <wighawag@gmail.com>
2020-07-01 10:37:02 +0100

    update buidler-deploy to ^0.4.0-beta.1

--- deploy/000_deploy_greeter.ts
+++ deploy/000_deploy_greeter.ts
@@ -6,6 +6,6 @@ const func: DeployFunction = async function(bre : BuidlerRuntimeEnvironment) {
 
     const {deployer} = await getNamedAccounts();
     
-    await deploy("Greeter", {from: deployer}, "Greeter", "hi");
+    await deploy("Greeter", {from: deployer, args: ["hi"]});
 }
 export default func;
